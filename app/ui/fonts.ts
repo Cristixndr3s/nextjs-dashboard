004b3d8a586ed3aa6816ceac0d660b7596370f8b
Cristixndr3s <cristiandres17@gmail.com>
2025-07-01 20:35:19 -0500

    capitulo 6

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,10 @@
-import { Inter } from 'next/font/google'; 
-export const inter = Inter({ subsets: ['latin'] });
+import { Lusitana, Inter } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
